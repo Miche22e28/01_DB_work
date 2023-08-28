0290992fc3c8fca2e820491591dabc0c1d3d19d9
Miche22e28 <mbergamasco11@googlemail.com>
2023-08-28 08:50:33 +0000

    02_SQL_Tables_Aktualisiert

--- 02_SQL_Tables/02b_db_table_SQL.sql
+++ 02_SQL_Tables/02b_db_table_SQL.sql
@@ -1,8 +1,9 @@
 /* ------  Strukturen ----- */
 
-/* Kommentar 1 */
-#  Kommentar 2
--- Kommentar 3
+/* 
+    NULL wird nicht mehr zugelassen
+    DEFAULT-Werte eintragen
+*/
 
 /* Datenbanken auf Server anzeigen */
 SHOW DATABASES;
@@ -19,8 +20,8 @@ USE boo;
 /* Tabelle cats anlegen, falls noch nicht vorhanden */
 CREATE TABLE IF NOT EXISTS cats
 (
-    name VARCHAR(20),
-    age INT
+    name VARCHAR(20) NOT NULL DEFAULT "TBA",
+    age INT NOT NULL DEFAULT 0
 );
 
 
@@ -34,6 +35,10 @@ DESCRIBE cats;
 INSERT INTO cats(name,age) VALUES ("Grizabella",29);
 INSERT INTO cats(name,age) VALUES (35,"Alonzo");
 INSERT INTO cats VALUES();
+
+-- ABER: Doppelte Datensetze werde zugelassen!
+INSERT INTO cats(age,name) VALUES (35,"Alonzo");
+INSERT INTO cats(age,name) VALUES (35,"Alonzo");
 
 /*----Inhalte der Tabelle anzeigen----*/
 SELECT * FROM cats;
